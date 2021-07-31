c32e34fa04b8a9007ada8cbd2aca821290a889f6
s-ly <sergeylysov@outlook.com>
2021-08-01 00:46:01 +0300

    Теперь регистр букв не важен, сообщение про ответ не выводится в чат, добавил новые слова в словарь, переименовал /test на /go.

--- EnglishTrainingBot.py
+++ EnglishTrainingBot.py
@@ -5,16 +5,16 @@ from aiogram import Bot, Dispatcher, executor, types
 
 # дополнительно для форматирования
 from aiogram.utils.markdown import text, bold, italic, code, underline, strikethrough
-import MyToken # содержит токен
-import EnglishTraining
-import random   # для раднома
+import MyToken         # содержит токен
+import EnglishTraining # мой модуль, читает словарь из файла
+import random          # для раднома
 
-# Импрорт токена из файла MyToken.py (лежит в раб каталоге)
-# Файл MyToken.py содержит две строки:
-# myToken = 'тут токен'
-# testToken = 'тут токен'
-# При разработке использеум test, для работы my.
-# в git его игнорируем, а в место пушим зашифрованный архив.
+""" Импрорт токена из файла MyToken.py (лежит в раб каталоге)
+Файл MyToken.py содержит две строки:
+myToken = 'тут токен'
+testToken = 'тут токен'
+При разработке использеум test, для работы my.
+в git его игнорируем, а в место пушим зашифрованный архив."""
 
 API_TOKEN = MyToken.myToken # рабочий бот
 # API_TOKEN = MyToken.testToken # тестовый бот
@@ -34,9 +34,9 @@ async def send_welcome(message: types.Message):
 
 
 
-@dp.message_handler(commands=['test'])
+@dp.message_handler(commands=['go'])
 async def send_welcome2(message: types.Message):
-    """ Отвечает на команды /test """
+    """ Отвечает на команды /go """
     languageSelection = random.randint(0, 1) # случайный выбор языка
     
     # приставка, подставляемая к слову id, что бы понимать направление перевода.
@@ -44,8 +44,6 @@ async def send_welcome2(message: types.Message):
 
     dictLen = len(EnglishTraining.dict_words)        # кол-во слов в словаре
     dictRandomID = (random.randrange(dictLen)) + 1   # случайное слово
-    # await message.answer("id:" + str(dictRandomID))
-    # await message.answer(str(EnglishTraining.dict_words[dictRandomID]))
 
     # фрмирование строки вопроса изходя из случайно выбранного направления перевода
     if languageSelection == 0:
@@ -55,16 +53,14 @@ async def send_welcome2(message: types.Message):
         questionWord = (EnglishTraining.dict_words[dictRandomID])[2]
         languageSelectionPrefix = "Rus"
     await message.answer(languageSelectionPrefix + "Id:" +  str(dictRandomID) + " " + questionWord)
-        # await message.answer("Id:" + str(dictRandomID) + " " +
-        # str(EnglishTraining.dict_words[dictRandomID]))
     
-
 
 
 @dp.message_handler(commands=['dict'])
 async def send_test(message: types.Message):
     """ Отвечает на команды /dict.
     Берёт из модуля EnglishTraining список слов dict_words. 
+    Метод code() делает шрифт моноширным.
     Формирует строки и конкатинирует их с символом новой строки.
     Когда количетво как бы строк subStrokeSum в общей строке достигает максимума,
     выводит в телеграм, затем поновой бежит по списку слов. """
@@ -74,9 +70,8 @@ async def send_test(message: types.Message):
     subStrokeSum = 50  # кол-во подстрок в строке
     len_col = 15       # ширина колонок
     
-    for i in EnglishTraining.dict_words:
-        # сётчик строк
-        sum_strok = sum_strok + 1
+    for i in EnglishTraining.dict_words:        
+        sum_strok = sum_strok + 1 # сётчик строк
 
         # вычисляем недостающее кол-во пробелов для ширины колонок
         dobavka_col_0 = len_col - len(i[0])
@@ -100,13 +95,16 @@ async def send_test(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def send_test(message: types.Message):
     """ Отвечает на команды /help. """
-    await message.answer("English Training\nVersion 2021.07.27\n" + 
+    await message.answer("English Training\nVersion 2021.08.01\n" + 
     "Тренируй английские слова!\n\nСписок команд (вводи с палочкой):\n" + 
-    "/start - запуск, приветствие\n" + 
+    "/start - запуск бота, приветствие\n" + 
     "/help - справка\n" + 
     "/dict - показать словарь\n" +
-    "/test - тестовая функция\n\n" +
-    "Для удобства просмотра словаря, поверни телефон горизонтально.\n")    
+    "/go - начать тренировку\n\n" +
+    "После запуска тренировки бот выдаёт слово для перевода, чтобы ответить, " +
+    "нажми на слово и выбери 'ОТВЕТИТЬ', потом пиши ответ и отправляй. " +
+    "Для удобства просмотра словаря, поверни телефон горизонтально. " +
+    "Если бот не даёт слово, снова введи /go.")    
 
 
 
@@ -118,10 +116,8 @@ async def send_welcome(message: types.Message):
     responseSign = "" # признак ответа
 
     # await message.reply(message.text)
-    if (message.reply_to_message != None):
-        await message.answer("Зафиксирован ответ на сообщение: " + message.reply_to_message.text)
-        
-        print("\nЗафиксирован ответ")
+    if (message.reply_to_message != None):        
+        print("\nЗафиксирован ответ на сообщение: " + message.reply_to_message.text)
         print(message.reply_to_message.date)
         print(message)
 
@@ -133,6 +129,7 @@ async def send_welcome(message: types.Message):
             questionBotWordId = int((message.reply_to_message.text)[6:endId]) # срез id 
             questionBotWord = (message.reply_to_message.text)[(endId + 1):] # срез загадываемого слова
             answerUser = message.text # ответ пользователя
+            answerUser = answerUser.lower() # всё с маленькой буквы
             
             print(str(questionBotWordId)) # печать id загадываемого слова
             print(questionBotWord) # печать загадываемого слова
@@ -147,7 +144,7 @@ async def send_welcome(message: types.Message):
                 await message.answer("Ошибка, правильный ответ: " + 
                 (EnglishTraining.dict_words[questionBotWordId])[2]) 
 
-            await send_welcome2(message) # поновой, как буддто пользователь ввёл "/test"
+            await send_welcome2(message) # поновой, как буддто пользователь ввёл "/go"
 
         if (message.reply_to_message.text).find("RusId:") == 0:   
             print("Направление перевода с Rus на Eng") 
@@ -156,6 +153,7 @@ async def send_welcome(message: types.Message):
             questionBotWordId = int((message.reply_to_message.text)[6:endId]) # срез id 
             questionBotWord = (message.reply_to_message.text)[(endId + 1):] # срез загадываемого слова
             answerUser = message.text # ответ пользователя
+            answerUser = answerUser.lower() # всё с маленькой буквы
             
             print(str(questionBotWordId)) # печать id загадываемого слова
             print(questionBotWord) # печать загадываемого слова
@@ -170,7 +168,7 @@ async def send_welcome(message: types.Message):
                 await message.answer("Ошибка, правильный ответ: " + 
                 (EnglishTraining.dict_words[questionBotWordId])[0])  
 
-            await send_welcome2(message) # поновой, как буддто пользователь ввёл "/test"
+            await send_welcome2(message) # поновой, как буддто пользователь ввёл "/go"
         
         
         # print(message.reply_to_message)
