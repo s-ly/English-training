5da528923684c003e70ca68fbc1bca1f43efa762
s-ly <sergeylysov@outlook.com>
2021-10-17 22:54:07 +0300

    добавлена кнопка подсказка, словарь 800 слов

--- EnglishTrainingBot.py
+++ EnglishTrainingBot.py
@@ -6,10 +6,16 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext # хранение контекста
 from aiogram.contrib.fsm_storage.memory import MemoryStorage # место для хранения контекста в ОЗУ
 
+# необходимо делать проверку на полное совпадение текста. через специальный фильтр Text 
+from aiogram.dispatcher.filters import Text
+
+# для кнопок
+from aiogram.types.reply_keyboard import KeyboardButton, ReplyKeyboardRemove, ReplyKeyboardMarkup
+
 # дополнительно для форматирования
 from aiogram.utils.markdown import text, bold, italic, code, underline, strikethrough
 import MyToken         # содержит токен
-import dict            # мой модуль, читает словарь из файла
+import func            # мой модуль, содержит функции для бота
 import Texts           # мой модуль, хранит текст
 import log             # мой модуль, лог
 import random          # для раднома
@@ -31,16 +37,62 @@ dp = Dispatcher(bot, storage=storage)
 
 
 
+# создаём коавиатуру и добавляем кнопки
+# resize_keyboard=True - уменьшает кнопки
+# one_time_keyboard=True - скрыть кнопку после нажатия
+# keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton('* Не помню *')
+button2 = KeyboardButton('* Подсказка *')
+keyboard.add(button1, button2)
+
+
+# (Text(equals="* Не помню *")) проверяется полное сооьветствие с текстом
+# ReplyKeyboardRemove() - удаляет клавиатуру из меню
+@dp.message_handler(Text(equals="* Не помню *"))
+async def with_puree(message: types.Message, state: FSMContext):
+    """ Отвечает на текс '* Не помню *'"""
+    allUserData = await state.get_data() # загружаем статусы пользователя
+    questionWord = allUserData['questionWord'] # текущий вопрос
+    currentDictStroke = func.dict_words[allUserData['idWord']] # текущая строка словаря 
+    
+    # Подсчёт кол-ва синонимов в строке словаря (мой метод) 
+    len_i = await func.SumSynonym(currentDictStroke) 
+
+    # формируем строку правильного ответа в зависимости от кол-ва синонимов (len_i)
+    correctAnswer = await func.CorrectAnswer(currentDictStroke, len_i)
+
+    await message.answer(correctAnswer) # правильный ответ 
+    await message.answer('? ' + str(questionWord)) # повтор вопроса
+
+
+
+# (Text(equals="* Подсказка *")) проверяется полное сооьветствие с текстом
+@dp.message_handler(Text(equals="* Подсказка *"))
+async def with_puree(message: types.Message, state: FSMContext):
+    """ Отвечает на текс '* Подсказка *'"""
+    allUserData = await state.get_data() # загружаем статусы пользователя
+    currentDictStroke = func.dict_words[allUserData['idWord']] # текущая строка словаря 
+    translatDir = allUserData['translatDir'] # текущее направление перевода
+    tooltip = await func.TooltipGenerator(currentDictStroke, translatDir) # генератор подсказок
+    await message.answer(tooltip) 
+
+
+
+# ReplyKeyboardRemove() - удаляет клавиатуру из меню
 @dp.message_handler(commands=['start'])
 async def send_start(message: types.Message, state: FSMContext):
     """ Отвечает на команды /start """
     # Второй параметр FSMContext хранит контекст 
-
-    # Инициализация контекста (данных пользователя)
-    await InItStateUser(message, state)
+    
+    await InItStateUser(message, state) # Инициализация контекста (данных пользователя)
 
     await message.answer("English Training\n" + 
-    "Тренируй английские слова!\nДля начала тренировки введи /go, справка /help (с палочкой).")
+    "Тренируй английские слова!\nДля начала тренировки введи /go, справка /help (с палочкой).",
+    reply_markup=ReplyKeyboardRemove())
+    await state.update_data(showkeyboard='true') # теперь клава буду вызвана при /go
+
+
 
 @dp.message_handler(commands=['status'])
 async def userStatus(message: types.Message, state: FSMContext):
@@ -65,36 +117,30 @@ async def send_welcome2(message: types.Message, state: FSMContext):
 
     # Инициализация контекста (данных пользователя)
     await InItStateUser(message, state)
+    allUserData = await state.get_data() # загружаем статусы пользователя
 
     dictRandomID = None # без этого появлялась ошибка выхода из диапазона (иногда)
     questionWord = None # обнуление старого вопроса (на всякий)
     randomDictStroke = None
-    languageSelection = random.randint(0, 1) # случайный выбор языка
-    
-    # приставка, подставляемая к слову id, что бы понимать направление перевода.
-    languageSelectionPrefix = ""
-
-    dictLen = len(dict.dict_words)                   # кол-во слов в словаре
+    languageSelection = random.randint(0, 1)         # случайный выбор языка
+    dictLen = len(func.dict_words)                   # кол-во слов в словаре
     dictRandomID = random.randint(0, dictLen - 1)    # случайное слово
     await state.update_data(idWord=dictRandomID)     # случайное слово в статус пользователя
-    randomDictStroke = dict.dict_words[dictRandomID] # случайная строка
+    randomDictStroke = func.dict_words[dictRandomID] # случайная строка
 
     # фрмирование строки вопроса изходя из случайно выбранного направления перевода
     # если с русского то учитывается кол-во синонимов
     if languageSelection == 0:
-        languageSelectionPrefix = "Eng"
-
         await state.update_data(translatDir='Eng') # направление перевода в статус пользователя
 
         questionWord = str(randomDictStroke[0])
-    elif languageSelection == 1:
-        languageSelectionPrefix = "Rus"
 
+    elif languageSelection == 1:
         await state.update_data(translatDir='Rus') # направление перевода в статус пользователя
 
         len_i = 0 # получаемое кол-во синонимов (обнуление)    
         # Подсчёт кол-ва синонимов в строке словаря (мой метод) 
-        len_i = await SumSynonym(randomDictStroke) 
+        len_i = await func.SumSynonym(randomDictStroke) 
         if len_i == 1:
             questionWord = str(randomDictStroke[2])
         elif len_i == 2:
@@ -109,22 +155,34 @@ async def send_welcome2(message: types.Message, state: FSMContext):
             questionWord = (str(randomDictStroke[2]) + ', ' + str(randomDictStroke[3]) 
             + ', ' + str(randomDictStroke[4]) + ', ' + str(randomDictStroke[5])
             + ', ' + str(randomDictStroke[6]))
-    # await message.answer(languageSelectionPrefix + "Id:" +  str(dictRandomID) + " " + str(questionWord))
-    await message.answer(str(questionWord))
+    
+    # Идея в том, что бы клавиатура всплыла только один раз, а потом не надоедала,
+    # а просто была в виде значка, для этого используем FSM параметр showkeyboard='true'.
+    if (allUserData['showkeyboard'] == 'true'):
+        await message.answer('? ' + str(questionWord),
+        reply_markup=keyboard) # параметр передаёт клавиатуру
+        
+        # подсказка про меню (с формаьтрованием)
+        await message.answer(italic('(кнопка с подсказкой -> квадратик справа)'),
+        parse_mode=types.ParseMode.MARKDOWN_V2) 
+
+        await state.update_data(showkeyboard='false') # больше показывать клавиатуру не надо
+    else:
+        await message.answer('? ' + str(questionWord)) # без клавиатуры
+
     await state.update_data(questionWord=str(questionWord)) # статус пользователя: вопрос
     await state.update_data(userStatus='userHasQuestion') # статус пользователя: пользователю задан вопрос
     
+    
 
-
+# Берёт из модуля func список слов dict_words. 
+# Метод code() делает шрифт моноширным.
+# Формирует строки и конкатинирует их с символом новой строки.
+# Когда количетво как бы строк subStrokeSum в общей строке достигает максимума,
+# выводит в телеграм, затем поновой бежит по списку слов. 
 @dp.message_handler(commands=['dict'])
 async def send_test(message: types.Message, state: FSMContext):
-    """ Отвечает на команды /dict."""
-    # Берёт из модуля dict список слов dict_words. 
-    # Метод code() делает шрифт моноширным.
-    # Формирует строки и конкатинирует их с символом новой строки.
-    # Когда количетво как бы строк subStrokeSum в общей строке достигает максимума,
-    # выводит в телеграм, затем поновой бежит по списку слов. 
-    
+    """ Отвечает на команды /dict."""    
     sum_all_strok = 0  # общее кол-во слов
     sum_strok = 0      # ограничитель печатаемых ботом строк
     dict_word_bot = '' # формируемая для отправки боту строка
@@ -132,7 +190,7 @@ async def send_test(message: types.Message, state: FSMContext):
     subStrokeSum = 50  # кол-во подстрок в строке
     len_col = 15       # ширина колонок
     
-    for i in dict.dict_words:        
+    for i in func.dict_words:        
         sum_strok = sum_strok + 1         # сётчик строк
         sum_all_strok = sum_all_strok + 1 # счётчик всех слов в словаре
 
@@ -140,7 +198,7 @@ async def send_test(message: types.Message, state: FSMContext):
         dobavka_col_0 = len_col - len(i[0])
 
         len_i = 0 # получаемое колво синонимов
-        len_i = await SumSynonym(i) # Подсчёт кол-ва синонимов в строке словаря (мой метод)
+        len_i = await func.SumSynonym(i) # Подсчёт кол-ва синонимов в строке словаря (мой метод)
 
         # формируем и добавляем строку со всеми колонками (без транскрипции)
         # в зависимости от кол-ва синонимов.
@@ -174,20 +232,22 @@ async def send_test(message: types.Message, state: FSMContext):
     sum_all_strok = 0 # обнуляем счётчик всех слов
 
 
-
+# ReplyKeyboardRemove() - удаляет клавиатуру из меню
 @dp.message_handler(commands=['help'])
 async def send_test(message: types.Message, state: FSMContext):
     """ Отвечает на команды /help. """
-    await message.answer(Texts.answerHelp) # текст из модуля    
+    await message.answer(Texts.answerHelp, reply_markup=ReplyKeyboardRemove()) # текст из модуля    
+    await state.update_data(showkeyboard='true') # теперь клава буду вызвана при /go
 
 
 
+# ReplyKeyboardRemove() - удаляет клавиатуру из меню
+# Проверяет, нет ли в сообщении ответа (кнопка ответить).
+# Если ответ есть, вызываем CheckingResponse(message) с передачей методу самого сообщения.
+# Иначе предлагаем пользователю посмотреть справку.
 @dp.message_handler()
 async def send_welcome(message: types.Message, state: FSMContext):
     """ Отвечает на любые сообщения."""
-    # Проверяет, нет ли в сообщении ответа (кнопка ответить).
-    # Если ответ есть, вызываем CheckingResponse(message) с передачей методу самого сообщения.
-    # Иначе предлагаем пользователю посмотреть справку.
 
     # логирование
     logUser = str(message.chat.username)
@@ -203,9 +263,11 @@ async def send_welcome(message: types.Message, state: FSMContext):
         if (allUserData['userStatus'] == 'userHasQuestion'):
             await CheckingResponseState(message, state)
         else:
-            await message.answer(Texts.miniHelp)  
+            await message.answer(Texts.miniHelp, reply_markup=ReplyKeyboardRemove())  
+            await state.update_data(showkeyboard='true') # теперь клава буду вызвана при /go
     else:
-        await message.answer(Texts.miniHelp)  
+        await message.answer(Texts.miniHelp, reply_markup=ReplyKeyboardRemove())  
+        await state.update_data(showkeyboard='true') # теперь клава буду вызвана при /go
 
 
 
@@ -215,40 +277,17 @@ async def CheckingResponseState(message, state):
     answerUser = message.text       # ответ пользователя
     answerUser = answerUser.lower() # всё с маленькой буквы
 
-    currentDictStroke = dict.dict_words[allUserData['idWord']] # текущая строка словаря 
+    currentDictStroke = func.dict_words[allUserData['idWord']] # текущая строка словаря 
     translatDir = allUserData['translatDir'] # текущее направление перевода
     questionWord = allUserData['questionWord'] # текущий вопрос
 
     len_i = 0 # получаемое кол-во синонимов (обнуление)
     
     # Подсчёт кол-ва синонимов в строке словаря (мой метод) 
-    len_i = await SumSynonym(currentDictStroke) 
+    len_i = await func.SumSynonym(currentDictStroke) 
 
     # формируем строку правильного ответа в зависимости от кол-ва синонимов (len_i)
-    correctAnswer = '' # ответ бота
-    if len_i == 1:
-        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
-        str(currentDictStroke[2]))
-
-    elif len_i == 2:
-        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
-        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]))
-
-    elif len_i == 3:
-        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
-        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
-        str(currentDictStroke[4]))
-
-    elif len_i == 4:
-        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
-        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
-        str(currentDictStroke[4]) + ', ' + str(currentDictStroke[5]))
-
-    elif len_i == 5:
-        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
-        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
-        str(currentDictStroke[4]) + ', ' + str(currentDictStroke[5]) + ', ' +
-        str(currentDictStroke[6]))
+    correctAnswer = await func.CorrectAnswer(currentDictStroke, len_i)
 
     # проверка ответа пользователья если с RUS на ENG
     if translatDir == 'Rus':
@@ -257,7 +296,7 @@ async def CheckingResponseState(message, state):
         else:
             await message.answer("Ошибка\n" + correctAnswer)             
             await message.answer("Попробуй ещё")
-            await message.answer(questionWord) # повтор вопроса
+            await message.answer('? ' + str(questionWord)) # повтор вопроса
             return # выход из метода
     
     # проверка ответа пользователья если с ENG на RUS в зависимости от кол-ва синонимов
@@ -268,7 +307,7 @@ async def CheckingResponseState(message, state):
             else:
                 await message.answer("Ошибка\n" + correctAnswer) 
                 await message.answer("Попробуй ещё")
-                await message.answer(questionWord) # повтор вопроса
+                await message.answer('? ' + str(questionWord)) # повтор вопроса
                 return # выход из метода
 
         elif len_i == 2:
@@ -277,7 +316,7 @@ async def CheckingResponseState(message, state):
             else:
                 await message.answer("Ошибка\n" + correctAnswer) 
                 await message.answer("Попробуй ещё")
-                await message.answer(questionWord) # повтор вопроса
+                await message.answer('? ' + str(questionWord)) # повтор вопроса 
                 return # выход из метода
 
         elif len_i == 3:
@@ -286,8 +325,8 @@ async def CheckingResponseState(message, state):
                 await message.answer("Правильно\n" + correctAnswer) 
             else:
                 await message.answer("Ошибка\n" + correctAnswer) 
-                await message.answer("Попробуй ещё")
-                await message.answer(questionWord) # повтор вопроса
+                await message.answer("Попробуй ещё")                
+                await message.answer('? ' + str(questionWord)) # повтор вопроса
                 return # выход из метода
   
         elif len_i == 4:
@@ -297,7 +336,7 @@ async def CheckingResponseState(message, state):
             else:
                 await message.answer("Ошибка\n" + correctAnswer) 
                 await message.answer("Попробуй ещё")
-                await message.answer(questionWord) # повтор вопроса
+                await message.answer('? ' + str(questionWord)) # повтор вопроса
                 return # выход из метода
         
         elif len_i == 5:
@@ -308,36 +347,38 @@ async def CheckingResponseState(message, state):
             else:
                 await message.answer("Ошибка\n" + correctAnswer) 
                 await message.answer("Попробуй ещё")
-                await message.answer(questionWord) # повтор вопроса
+                await message.answer('? ' + str(questionWord)) # повтор вопроса
                 return # выход из метода
 
     await send_welcome2(message, state) # поновой, как буддто пользователь ввёл "/go"
 
 
-async def SumSynonym(dict_string):
-    """Подсчёт кол-ва синонимов в строке словаря."""
-    # принимает строку словаря (список), возвращает число
-    # подсчитываем кол-во синонимов в каждой строке
-    # no_none_sub_i - кол-во непустых ячеек в строке
-    # len_i - получаемое колво синонимов
-    # Цикл бежит по строке и считает непустые ячейки, потом вычитаем 2, так как первые две ячейки
-    # это английское слово и транскрипция. 
-    no_none_sub_i = 0
-    len_i = 0
-    for sub_i in dict_string:
-        if(sub_i != ''):
-            no_none_sub_i = no_none_sub_i + 1
-    len_i = no_none_sub_i - 2
-    return len_i
     
 async def InItStateUser(message: types.Message, state: FSMContext):
     """ Инициирует данные пользователя """
     # Инициализация контекста (данных пользователя)
-    await state.update_data(userName=message.chat.username)
-    await state.update_data(userStatus='registr')
-    await state.update_data(idWord='no')
-    await state.update_data(translatDir='no')
-    await state.update_data(questionWord='no')
+    # если инициализации ещё небыло
+    allUserData = await state.get_data() # загружаем статусы пользователя
+
+    if ('userStatus' in allUserData):
+        # print('инициация уже была, ')
+        # обнуляем только то что нужно (кроме показа клавиатуры)
+        await state.update_data(userName=message.chat.username)
+        await state.update_data(userStatus='registr')
+        await state.update_data(idWord='no')
+        await state.update_data(translatDir='no')
+        await state.update_data(questionWord='no')
+        # print(str(allUserData['showkeyboard']))
+    else:
+        # print('инициируем')
+        # инициируем всё
+        await state.update_data(userName=message.chat.username)
+        await state.update_data(userStatus='registr')
+        await state.update_data(idWord='no')
+        await state.update_data(translatDir='no')
+        await state.update_data(questionWord='no')
+        await state.update_data(showkeyboard='true')
+
 
 
 if __name__ == '__main__':
---
+++ func.py
@@ -0,0 +1,74 @@
+# Модуль содержит функции для бота
+
+import csv      # для чтения таблици
+
+# читаем из таблици данные в наш список списков dict_words
+dict_words = []
+read_file = open('dict.csv', 'r')
+for row in csv.reader(read_file):
+    dict_words.append(row)
+read_file.close()
+
+
+
+async def CorrectAnswer(currentDictStroke, len_i):
+    """формируем строку правильного ответа в зависимости от кол-ва синонимов (len_i)"""
+    correctAnswer = '' # ответ бота
+    if len_i == 1:
+        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
+        str(currentDictStroke[2]))
+
+    elif len_i == 2:
+        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
+        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]))
+
+    elif len_i == 3:
+        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
+        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
+        str(currentDictStroke[4]))
+
+    elif len_i == 4:
+        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
+        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
+        str(currentDictStroke[4]) + ', ' + str(currentDictStroke[5]))
+
+    elif len_i == 5:
+        correctAnswer = (str(currentDictStroke[0]) + " [" + str(currentDictStroke[1]) + "] " +
+        str(currentDictStroke[2]) + ', ' + str(currentDictStroke[3]) + ', ' + 
+        str(currentDictStroke[4]) + ', ' + str(currentDictStroke[5]) + ', ' +
+        str(currentDictStroke[6]))
+
+    return correctAnswer
+
+
+
+# принимает строку словаря (список), возвращает число
+# подсчитываем кол-во синонимов в каждой строке
+# no_none_sub_i - кол-во непустых ячеек в строке
+# len_i - получаемое колво синонимов
+# Цикл бежит по строке и считает непустые ячейки, потом вычитаем 2, так как первые две ячейки
+# это английское слово и транскрипция. 
+async def SumSynonym(dict_string):
+    """Подсчёт кол-ва синонимов в строке словаря."""
+    no_none_sub_i = 0
+    len_i = 0
+    for sub_i in dict_string:
+        if(sub_i != ''):
+            no_none_sub_i = no_none_sub_i + 1
+    len_i = no_none_sub_i - 2
+    return len_i
+
+
+
+async def TooltipGenerator(currentDictStroke, translatDir):
+    """генератор подсказок"""
+    # tooltip = str(currentDictStroke) + str(translatDir)
+    if (translatDir == 'Rus'):
+        word = currentDictStroke[0] # английское слово
+    elif (translatDir == 'Eng'):
+        word = currentDictStroke[2] # русское слово
+
+    numberOfLetters = len(word) # длинна слова
+    firstLetter = word[0]       # первая буква слова
+    tooltip = firstLetter + ('*' * (numberOfLetters-1))  
+    return tooltip
